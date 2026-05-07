405acf351d3c6e53f4ae8bfd79ffddfed3dddfb6
Dan1ell1 <daniellisz.silva@gmail.com>
2026-05-07 14:07:26 -0300

    ajuste back

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="AgroAgenda API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://agro-agenda-seven.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
